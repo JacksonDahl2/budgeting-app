d12cf22baaad3b7783099225b18dd958c93f28ae
Jackson <jacksondahl27@gmail.com>
2023-11-27 19:50:38 -0500

    Refactored formatting, created a dockerfile for the backend

---
+++ frontend/src/Stuff.tsx
@@ -0,0 +1,9 @@
+import { Text } from 'react-native'
+
+const Stuff = () => {
+  return (
+    <Text>Test</Text>
+  )
+}
+
+export default Stuff
--- src/stuff.tsx
+++
@@ -1,7 +0,0 @@
-import { Text } from "react-native";
-
-const StateItem = () => {
-  return <Text> Test </Text>;
-};
-
-export default StateItem;
